611f1e734aa2c7e8203d2abc78e636fe665527cb
Bill Piedra <Bill@Bills-iMac-770.local>
2022-02-23 09:51:32 -0500

    sync contacts

--- utilities.js
+++ utilities.js
@@ -45,7 +45,10 @@ exports.getCCSyncData = function(accessToken, contacts){
             last: c.last_name
         };
 
-        for (var j=0; j<c.phone_numbers; j++){
+        var phoneLength = 0;
+        if (c.phone_numbers) phoneLength = c.phone_numbers.length;
+        for (var j=0; j<c.phoneLength; j++){
+
             if (c.phone_numbers[i].kind === "home"){
                 contact.phone_home = c.phone_numbers[i].phone_number;
             } else if (c.phone_numbers[i].kind === "work"){
@@ -56,7 +59,33 @@ exports.getCCSyncData = function(accessToken, contacts){
                 contact.phone_other = c.phone_numbers[i].phone_number;
             }
         }
+
+        var addressLength = 0;
+        if (c.street_addresses) addressLength = c.street_addresses.length;
+        for (var k=0; k<addressLength; k++){
+            
+            if (contact.street_addresses[k].kind === "home"){
+                c.address_home_street = contact.street_addresses[k].street;
+                c.address_home_city = contact.street_addresses[k].city;
+                c.address_home_state = contact.street_addresses[k].state;
+                c.address_home_zip = contact.street_addresses[k].postal_code;
+                c.address_home_country = contact.street_addresses[k].country;
+            } else if (contact.street_addresses[k].kind === "work"){
+                c.address_work_street = contact.street_addresses[k].street;
+                c.address_work_city = contact.street_addresses[k].city;
+                c.address_work_state = contact.street_addresses[k].state;
+                c.address_work_zip = contact.street_addresses[k].postal_code;
+                c.address_work_country = contact.street_addresses[k].country;
+            } else if (contact.street_addresses[k].kind === "other"){
+                c.address_other_street = contact.street_addresses[k].street;
+                c.address_other_city = contact.street_addresses[k].city;
+                c.address_other_state = contact.street_addresses[k].state;
+                c.address_other_zip = contact.street_addresses[k].postal_code;
+                c.address_other_country = contact.street_addresses[k].country;
+            }
+        }
         toSync.push(contact);
+
     }
     return toSync;
 }
