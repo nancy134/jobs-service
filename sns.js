68ea460dccc46a2628bc389e15b756641930abe6
Bill Piedra <Bill@Bills-iMac-770.local>
2022-02-28 09:22:58 -0500

    sync changes

--- sns.js
+++ sns.js
@@ -9,12 +9,7 @@ exports.syncCCContacts = function(contacts){
     for (var i=0; i<contacts.length; i++){
         var c = contacts[i];
 
-        var data = {
-            token: c.token,
-            email: c.email,
-            first: c.first,
-            last: c.last
-        }
+        var data = createSNSData(c);
 
         var params = {
             Message: JSON.stringify(data),
@@ -34,14 +29,10 @@ exports.syncCCContacts = function(contacts){
         var c = contacts[i];
         console.log(c.email);
 
-        var data = {
-            token: c.token,
-            email: c.email,
-            first: c.first,
-            last: c.last,
-            sparkId: c.sparkId,
-            customFieldId: c.customFieldId
-        }
+        var data = createSNSData(c);
+        data.sparkId = c.sparkId;
+        data.customFieldId = c.customFieldId;
+
 
         var params = {
             Message: JSON.stringify(data),
@@ -55,3 +46,34 @@ exports.syncCCContacts = function(contacts){
         });
     }
  }
+
+
+ function createSNSData(c){
+    var data = {};
+    if (c.token) data.token = c.token;
+    if (c.email) data.email = c.email;
+    if (c.first) data.first = c.first;
+    if (c.middle) data.middle = c.middle;
+    if (c.last) data.last = c.last;
+    if (c.phone_work) data.phone_work = c.phone_work;
+    if (c.phone_mobile) data.phone_mobile = c.phone_mobile;
+    if (c.phone_other) data.phone_other = c.phone_other;
+    if (c.phone_home) data.phone_home = c.phone_home;
+    if (c.phone_primary) data.phone_primary = c.phone_primary;
+    if (c.phone_pager) data.phone_pager = c.phone_pager;
+    
+    if (c.home_street) data.home_street = c.home_street;
+    if (c.home_city) data.home_city = c.home_city;
+    if (c.home_state) data.home_state = c.home_state;
+
+    if (c.work_zip) data.work_zip = c.work_zip;
+    return data;
+ 
+    if (c.work_street) data.work_street = c.work_street;
+    if (c.work_city) data.work_city = c.work_city;
+    if (c.work_state) data.work_state = c.work_state;
+    if (c.work_zip) data.work_zip = c.work_zip;
+ 
+ }
+
+
